3de08280cf54ecd0d7a62eca6718b8a98083dadf
YourlocalJay <125357195+YourlocalJay@users.noreply.github.com>
2025-02-05 15:44:12 -0800

    Update logging.py

--- aiqleads/utils/logging.py
+++ aiqleads/utils/logging.py
@@ -1,1 +1,20 @@
+import logging
 
+class Logger:
+    """Configurable logging utility for AIQLeads."""
+
+    @staticmethod
+    def get_logger(name: str, level: int = logging.INFO) -> logging.Logger:
+        """Returns a pre-configured logger."""
+        logger = logging.getLogger(name)
+        logger.setLevel(level)
+
+        if not logger.hasHandlers():
+            handler = logging.StreamHandler()
+            formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+            handler.setFormatter(formatter)
+            logger.addHandler(handler)
+
+        return logger
+
+logger = Logger.get_logger("AIQLeads")
