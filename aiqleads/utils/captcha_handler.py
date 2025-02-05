2508b502da54717cae84825d181db8418064bbfe
YourlocalJay <125357195+YourlocalJay@users.noreply.github.com>
2025-02-05 14:02:12 -0800

    Update captcha_handler.py

--- aiqleads/utils/captcha_handler.py
+++ aiqleads/utils/captcha_handler.py
@@ -1,1 +1,18 @@
-  
+from bs4 import BeautifulSoup
+
+class CaptchaExtractor:
+    """Extracts CAPTCHA image links from scraped HTML content."""
+
+    CAPTCHA_CLASSES = ["rc-image-tile-wrapper", "hcaptcha-challenge-image", "captcha", "challenge-image"]
+
+    @staticmethod
+    async def extract_captcha_image(response) -> Optional[str]:
+        """Extracts CAPTCHA image source from HTML response."""
+        try:
+            soup = BeautifulSoup(await response.text(), "html.parser")
+            for class_name in CaptchaExtractor.CAPTCHA_CLASSES:
+                img = soup.find("img", {"class": class_name})
+                if img and "src" in img.attrs:
+                    return img["src"]
+        except Exception:
+            return None
