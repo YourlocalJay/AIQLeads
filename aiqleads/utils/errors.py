fb79c3643f1108cc4f3365939f4eb658782a4d1e
YourlocalJay <125357195+YourlocalJay@users.noreply.github.com>
2025-02-05 14:09:33 -0800

    Update errors.py

--- aiqleads/utils/errors.py
+++ aiqleads/utils/errors.py
@@ -1,1 +1,15 @@
+class ScraperError(Exception):
+    """Base exception for scraper-related errors."""
+    pass
 
+class RateLimitExceeded(ScraperError):
+    """Raised when rate limit is exceeded."""
+    pass
+
+class NetworkError(ScraperError):
+    """Raised when network requests fail."""
+    pass
+
+class ParseError(ScraperError):
+    """Raised when data parsing fails."""
+    pass
